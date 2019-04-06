29b99de5907ae727529163122421b439a7f3d37f
jaebooker <jaesonbooker@gmail.com>
2019-04-06 15:45:38 -0700

    working on uploads

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, redirect, request, render_template, session, url_for
 from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+import os
+#app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
 app = Flask(__name__)
 dropzone = Dropzone(app)
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-#app.config['DROPZONE_REDIRECT_VIEW'] = '?'
+app.config['DROPZONE_REDIRECT_VIEW'] = '/doners'
+app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)
 doners = [{
     'name': 'Jacklebees',
     'items': [{'name':'my item', 'price': 15.99 }]
@@ -19,6 +26,12 @@ def home():
 #post /sotre data: {name :}
 @app.route('/doner', methods=['POST'])
 def create_doner():
+    if request.method == 'POST':
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            print (file.filename)
+        return "uploading..."
     request_data = request.get_json()
     new_doner = {
         'name':request_data['name'],
