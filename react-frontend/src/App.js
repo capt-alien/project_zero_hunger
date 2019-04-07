d8194b41c76ba08ad5e0327b1d98e5a876913fd0
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-04-06 18:40:09 -0700

    react + flask is working

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p> My Token = {window.token} </p>
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ static/react/precache-manifest.2f500076c2d4bb63b85f2e71e9dafa05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "74646a3272612d26ea7e",
+    "url": "/static/react/js/main.68ead8e1.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/react/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "74646a3272612d26ea7e",
+    "url": "/static/react/css/main.f1115521.chunk.css"
+  },
+  {
+    "revision": "f2044a0bca69db9f6c768a8a271cbb9a",
+    "url": "/static/react/../templates/index.html"
+  }
+];
