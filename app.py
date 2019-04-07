4560ccc5888836e0b7a46efce72e5a1e168cdf2d
capt-alien <eric.botcher@presidio.edu>
2019-04-06 21:06:57 -0700

    item model working

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask_jwt_extended import JWTManager
 #mods
 from db import db
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_claims, get_jwt_identity, jwt_optional, fresh_jwt_required
+
 
 
 #app
@@ -15,9 +17,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-# app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
-# app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
-# app.secret_key = 'alien'  # could do app.config['JWT_SECRET_KEY'] if we prefer
+app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
+app.secret_key = 'alien'  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 @app.before_first_request
@@ -38,9 +40,10 @@ class DonerModel(db.Model):
 
 #nested resources
     # items = db.relationship(ItemModel, lazy='dynamic')
-    def __init__(self, name):
+
+    def __init__(self, name, address):
         self.name = name
-        # self.address = address
+        self.address = address
         # self.email = email
 
 
@@ -76,10 +79,15 @@ class ItemModel(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    quantity = db.column(db.Intiger, required=True)
+    quantity = db.Column(db.Integer)
     # Forign keys
-    doner_id = db.Column(db.Integer, db.ForeignKey('doner.id'))
-    doner = db.relationship('DonerModel')
+    # doner_id = db.Column(db.Integer, db.ForeignKey('doner.id'))
+    # doner = db.relationship('DonerModel')
+
+    def __init__(self, name, quantity, doner_id):
+        self.name = name
+        self.quantity = quantity
+        self.doner_id = doner_id
 
     def json(self):
         return {
@@ -109,6 +117,13 @@ class ItemModel(db.Model):
 
 # Doner Resource
 class Doner(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('address',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
     def get(self, name):
         doner = DonerModel.find_by_name(name)
         if doner:
@@ -121,15 +136,18 @@ class Doner(Resource):
         if DonerModel.find_by_name(name):
             return {'message': "a Doner with name '{}'' already exists.".format(name)}, 400
             # if not instantiate it
-        doner = DonerModel(name)
-        print("test", doner)
+        data = Doner.parser.parse_args()
+        doner = DonerModel(name, **data)
+        # print("test", doner['name'], doner['address'])
         # store in db
         try:
             doner.save_to_db()
-            print("test:  saved to DB")
         except:
             return {"message": "An error occurred creting the doner."}, 500
         return doner.json(), 201
+
+
+
 
 
     def delete(self, name):
@@ -147,17 +165,75 @@ class DonerList(Resource):
 # item Resource
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
+    parser.add_argument('quantity',
+                        type=int,
                         required=True,
                         help="This field cannot be left blank!"
                         )
-    parser.add_argument('store_id',
+    parser.add_argument('doner_id',
                         type=int,
                         required=True,
-                        help="Every item needs a store_id."
+                        help="Every item needs a doner_id."
+                        )
 
+    # @jwt_required  # No longer needs brackets
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
 
+    # @fresh_jwt_required
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = ItemModel(name, **data)
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
+        return item.json(), 201
+
+    # @jwt_required
+    def delete(self, name):
+        # claims = get_jwt_claims()
+        # if not claims['is_admin']:
+        #     return {'message': 'Admin privilege required.'}, 401
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.quantity = data['quantity']
+        else:
+            item = ItemModel(name, **data)
+        item.save_to_db()
+        return item.json()
+
+class ItemList(Resource):
+    @jwt_optional
+    def get(self):
+        """
+        Here we get the JWT identity, and then if the user is logged in (we were able to get an identity)
+        we return the entire item list.
+        Otherwise we just return the item names.
+        This could be done with e.g. see orders that have been placed, but not see details about the orders
+        unless the user has logged in.
+        """
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you log in.'
+            }, 200
 
 
 
@@ -166,6 +242,8 @@ class Item(Resource):
 #Routes
 api.add_resource(Doner, '/doner/<string:name>')
 api.add_resource(DonerList, '/')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
 
